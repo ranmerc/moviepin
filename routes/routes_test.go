272595bbccb8fba856e9876bda211a71f99a1489
Kamran Ansari <ranmerc@outlook.com>
2024-02-23 16:31:27 +0530

    test: add tests for routes

---
+++ routes/routes_test.go
@@ -0,0 +1,46 @@
+package routes
+
+import (
+	"net/http"
+	"testing"
+
+	"moviepin/handlers"
+	"moviepin/mock"
+)
+
+func TestNewRoutes(t *testing.T) {
+	mockService := &mock.ServiceMock{}
+
+	todoHandler := handlers.NewMovieHandler(mockService)
+	got := NewRoutes(todoHandler)
+
+	expectedRoutes := []struct {
+		Pattern string
+		Method  string
+	}{
+		{"/health", http.MethodGet},
+		{"/movies", http.MethodGet},
+		{"/movies/:movieID", http.MethodGet},
+		{"/movies", http.MethodPost},
+		{"/movies/:movieID", http.MethodPatch},
+		{"/movies/:movieID", http.MethodDelete},
+		{"/movies/:movieID", http.MethodPut},
+		{"/movies", http.MethodPut},
+		{"/movies/:movieID/rating", http.MethodGet},
+	}
+
+	t.Run("all routes are present", func(t *testing.T) {
+		for i := 0; i < min(len(got), len(expectedRoutes)); i++ {
+			got := got[i]
+			want := expectedRoutes[i]
+
+			if got.Pattern != want.Pattern {
+				t.Errorf("pattern expectations mismatched: \n want: %v \n got: %v", got.Pattern, want.Pattern)
+			}
+
+			if got.Method != want.Method {
+				t.Errorf("method expectations mismatched: \n want: %v \n got: %v", got.Method, want.Method)
+			}
+		}
+	})
+}
